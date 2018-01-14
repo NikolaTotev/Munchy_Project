ff56f65f3436dfe7a542a25695ff61a5631c23ce
Nikola Totev <nikola.r.totev@gmial.com>
2018-01-14 19:41:28 +0200

    UI updates

--- MunchyUI_Prototype/MainWindow.xaml.cs
+++ MunchyUI_Prototype/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Nikola.Munchy.MunchyAPI;
 namespace MunchyUI_Prototype
 {
     /// <summary>
@@ -20,9 +20,46 @@ namespace MunchyUI_Prototype
     /// </summary>
     public partial class MainWindow : Window
     {
+        string UserFile = @"d:\Desktop\USER5.json";
+        string DefaultUserFile = @"d:\Desktop\DEFAULT_USER.json";
+
+        string UserFridgeFile = @"d:\Desktop\USER_F.json";
+        string DefaultFridgeFile = @"d:\Desktop\DEFAULT_FRIDGE.json";
+
+        string FoodDefFile = @"d:\Desktop\FoodData.json";
+        string RecipeDatabase = @"d:\Desktop\Recipes.json";
+
+        ProgramManager CurrentManager;
+        
         public MainWindow()
         {
             InitializeComponent();
+            CurrentManager = new ProgramManager(UserFile, UserFridgeFile, DefaultFridgeFile, DefaultUserFile, RecipeDatabase, FoodDefFile);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }                    
+
+        private void btn_SeeAllSavedRecipes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SuggestRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_ShowRecipe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Showfridge_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
