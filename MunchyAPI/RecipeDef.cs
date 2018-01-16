c6ebcaf534826875b487e978b231b1a28768daa5
Nikola Totev <nikola.r.totev@gmial.com>
2018-01-16 21:02:13 +0200

    Last stable Version

--- MunchyAPI/RecipeDef.cs
+++ MunchyAPI/RecipeDef.cs
@@ -10,7 +10,8 @@ namespace Nikola.Munchy.MunchyAPI
     {
         public string TypeOfFood { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Directions { get; set; }
+        public int TimeToCook { get; set; }
         public List<string> Ingredients { get; set; }
         public List<float> Amounts { get; set; }
         public List<string> TimeTags { get; set; }
--- MunchyUI_Prototype/MainWindow.xaml.cs
+++ MunchyUI_Prototype/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace MunchyUI_Prototype
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Save File Locations
         string UserFile = @"d:\Desktop\USER5.json";
         string DefaultUserFile = @"d:\Desktop\DEFAULT_USER.json";
 
@@ -29,21 +30,35 @@ namespace MunchyUI_Prototype
         string FoodDefFile = @"d:\Desktop\FoodData.json";
         string RecipeDatabase = @"d:\Desktop\Recipes.json";
 
+        // A list of checkboxes that are used for saving the users settings and preferences
         List<CheckBox> SettingOptions;
+
+
+        // This list is used for populating the Ingredients ListView in the UI.
+        List<FoodDef> RecipeIngredientList;
+
         RecipeDef SuggestedRecipe = new RecipeDef();
         ProgramManager CurrentManager;
+
+        // RecipeImage handles the recipe image on the full recipe view aswell as the recipe view on the main menu.
         ImageBrush RecipeImage = new ImageBrush();
-        List<FoodDef> RecipeIngredientList;
+
 
         public MainWindow()
         {
-            InitializeComponent();  
+            InitializeComponent();
             CurrentManager = new ProgramManager(UserFile, UserFridgeFile, DefaultFridgeFile, DefaultUserFile, RecipeDatabase, FoodDefFile);
             SettingOptions = new List<CheckBox> { cb_Vegan, cb_Vegetarian, cb_Diabetic, cb_Eggs, cb_Dairy, cb_Fish, cb_Nuts, cb_Gluten, cb_Soy };
+            RecipeIngredientList = new List<FoodDef>();
+            PopulateFridgeUI();
+            SuggestRecipe();                                  
+        }
 
-             RecipeIngredientList = new List<FoodDef>();
-
-
+        /// <summary>
+        /// Adds all the elements in the users fridge to the listbox in the UI. Function is called on program start.
+        /// </summary>
+        private void PopulateFridgeUI()
+        {
             if (CurrentManager.User.UserFridge.UsersFoods.Count > 0)
             {
                 foreach (KeyValuePair<string, FoodDef> element in CurrentManager.User.UserFridge.UsersFoods)
@@ -51,26 +66,26 @@ namespace MunchyUI_Prototype
                     lb_FoodList.Items.Add(element.Key);
 
                     if (lb_Fridge.Items.Count < 10)
-                        lb_Fridge.Items.Add(element.Key);                   
+                        lb_Fridge.Items.Add(element.Key);
                 }
             }
-            SuggestRecipe();
-
-
-
         }
 
+        /// <summary>
+        /// Adds recipe information to the full recipe view. 
+        /// Function is called when the recipe view is opened, or when a new recipe is suggested.
+        /// </summary>
         private void AddRecipeIngredientsToListView()
         {
-            
             foreach (string ingredient in SuggestedRecipe.Ingredients)
             {
                 RecipeIngredientList.Add(new FoodDef() { Name = CurrentManager.FoodManag.Foods[ingredient].Name, Amount = SuggestedRecipe.Amounts[SuggestedRecipe.Ingredients.IndexOf(ingredient)] });
-            } 
-
-
+            }
 
             lv_Ingredients.ItemsSource = RecipeIngredientList;
+            tB_Directions.Text = SuggestedRecipe.Directions.ToString();
+            tB_RecipeTitle.Text = SuggestedRecipe.Name.ToString();
+            tB_TimeToCook.Text = SuggestedRecipe.TimeToCook.ToString();
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -78,31 +93,50 @@ namespace MunchyUI_Prototype
 
         }
 
+        /// <summary>
+        /// Shows all recipes saved by the user. Function is called on button press
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_SeeAllSavedRecipes_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Calls SuggestRecipe function on button click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_SuggestRecipe_Click(object sender, RoutedEventArgs e)
         {
             SuggestRecipe();
         }
 
+        /// <summary>
+        /// Shows/ Hides full recipe view and populates the UI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_ShowRecipe_Click(object sender, RoutedEventArgs e)
         {
             if (p_FullRecipeView.Visibility == Visibility.Hidden)
             {
+                RecipeImage.ImageSource = new BitmapImage(new Uri(SuggestedRecipe.ImageFile, UriKind.Relative));
+                img_RecipeImage.Fill = RecipeImage;
                 p_FullRecipeView.Visibility = Visibility.Visible;
             }
             else
             {
                 p_FullRecipeView.Visibility = Visibility.Hidden;
             }
-
-            RecipeImage.ImageSource = new BitmapImage(new Uri(SuggestedRecipe.ImageFile, UriKind.Relative));
-            img_RecipeImage.Fill = RecipeImage;
         }
 
+        /// <summary>
+        /// Opens full fridge view and populates the summary for the fridge.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_Showfridge_Click(object sender, RoutedEventArgs e)
         {
             if (p_UserFoods.Visibility == Visibility.Hidden)
@@ -113,14 +147,13 @@ namespace MunchyUI_Prototype
             {
                 p_UserFoods.Visibility = Visibility.Hidden;
             }
-
         }
 
-        private void btn_SaveSettings_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Opens user settings panel and populates the current active settings.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ShowSettings(object sender, MouseButtonEventArgs e)
         {
             tb_NameInput.Text = CurrentManager.User.UserName;
@@ -147,27 +180,27 @@ namespace MunchyUI_Prototype
             {
                 p_Settings.Visibility = Visibility.Hidden;
             }
-
-
         }
 
-        private void cb_Nuts_Copy1_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void cb_Nuts_Copy_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Saves users settings. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
+            int parsedValue;
             if (!string.IsNullOrWhiteSpace(tb_NameInput.Text) && !string.IsNullOrWhiteSpace(tb_AgeInput.Text) && !string.IsNullOrWhiteSpace(tb_WeightInput.Text))
             {
                 CurrentManager.User.UserName = tb_NameInput.Text;
-                CurrentManager.User.Age = int.Parse(tb_AgeInput.Text);
-                CurrentManager.User.Weight = int.Parse(tb_WeightInput.Text);
+
+                if (int.TryParse(tb_AgeInput.Text, out parsedValue))
+                    CurrentManager.User.Age = int.Parse(tb_AgeInput.Text);
+                else MessageBox.Show("Age is a number value!");
+
+                if (int.TryParse(tb_WeightInput.Text, out parsedValue))
+                    CurrentManager.User.Weight = int.Parse(tb_WeightInput.Text);
+                else MessageBox.Show("Weight is a number value!");
             }
 
             if (rb_Male.IsChecked == true)
@@ -208,9 +241,7 @@ namespace MunchyUI_Prototype
             CurrentManager.SaveUser();
             CurrentManager.User.CalculateIndex();
             CurrentManager.SaveUser();
-
             tB_UserName.Text = CurrentManager.User.UserName;
-
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
@@ -218,6 +249,11 @@ namespace MunchyUI_Prototype
 
         }
 
+        /// <summary>
+        /// Removes an item from the fridge UI and the user fridge.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_RemoveItem_Click(object sender, RoutedEventArgs e)
         {
             CurrentManager.User.UserFridge.RemoveFromFridge(lb_FoodList.SelectedItem.ToString());
@@ -231,11 +267,19 @@ namespace MunchyUI_Prototype
             }
         }
 
+        /// <summary>
+        /// Adds item to the fridge. User searches for item via the search panel.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_AddItem_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Suggests a recipe based on the time of day. Recipe sorting is handled in the back end.
+        /// </summary>
         private void SuggestRecipe()
         {
             if (DateTime.Now.Hour > 7 && DateTime.Now.Hour < 11)
@@ -263,10 +307,8 @@ namespace MunchyUI_Prototype
                     SuggestedRecipe = CurrentManager.RecipieManag.Recipies[CurrentManager.RecipieManag.Dinner[0]];
                 }
             }
-
             tB_RecipeName.Text = SuggestedRecipe.Name;
             AddRecipeIngredientsToListView();
-
         }
     }
 }
